c7485dd595b1a1c0459ea2bf897a2ad1a7a8462f
Muhammad Al-Habib Ouadhour <HOuadhour@yandex.com>
2018-07-26 18:39:48 +0100

    Fixing some things in the install script file

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/bash
-
-echo "\nChanging directoy to $HOME/.cache"
+echo ""
+echo "Changing directoy to $HOME/.cache"
+echo ""
 cd $HOME/.cache
+
+if [ -d $HOME/.cache/Sunrise ]; then
+    echo "Old directory has been found, and it has been deleted."
+    rm -rf $HOME/.cache/Sunrise 
+fi
 git clone https://github.com/HOuadhour/Sunrise.git
 
-echo "\nCreating a symlink of sunrise in /usr/local/bin"
-until sudo ln -s $HOME/.cache/Sunrise/sunrise.pyw /usr/local/bin/sunrise
-do
-    echo "Please check your password, Try again"
-done
-echo "\nSymlink has been created successfully."
-
-echo "\nCreating a desktop file."
+echo ""
+echo "Creating a symlink of sunrise in /usr/local/bin"
+echo ""
+if [ -L /usr/local/bin/sunrise ]; then
+    echo "Old symlink has been found, and it has been deleted."
+    echo ""
+    sudo rm -rf /usr/local/bin/sunrise
+fi
+sudo ln -s $HOME/.cache/Sunrise/sunrise.pyw /usr/local/bin/sunrise
+echo "Symlink has been created successfully."
+echo ""
+ 
+echo "Creating a desktop file."
+echo ""
+if [ -f $HOME/.local/share/applications/sunrise.desktop ]; then
+    echo "Old desktop file has been found, and it has been deleted."
+    echo ""
+    sudo rm -rf $HOME/.local/share/applications/sunrise.desktop
+fi
 cp $HOME/.cache/Sunrise/sunrise.desktop $HOME/.local/share/applications/
-echo "\nAdding the execute attribute of the file to the current user"
+echo "Adding the execute attribute of the file to the current user"
+echo ""
 chmod u+x $HOME/.local/share/applications/sunrise.desktop
-
 
